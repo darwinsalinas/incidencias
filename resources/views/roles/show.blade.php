c9789a156c0e94e76b085a612cc736c84b596f51
Jorge Velasquez <georgevelazquez616@gmail.com>
2019-04-30 20:51:57 -0600

    nuevo mensaje

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -1,37 +1,90 @@
 @extends('layouts.estilos')
-
 @section('content')
-<h4><strong>Rol: </strong>{{$role->name}}</h4>
-<table class="table">
-    <thead>
-        <tr>
-            <th>Usuarios</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($users as $user)
-        <tr>
-            <td scope="row">{{$user->name}}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                 <h4><strong>Rol: </strong>{{$role->name}}</h4>
+            </div>
+            
+        </div>
+    </div>
+</div>
 
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>Permisos</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($permisos as $permiso)
-        <tr>
-            <td scope="row">{{$permiso->name}}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-@endsection
+<div class="row">
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                 <h4>Ususarios</h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                             <th>Usuarios</th>
+                                <th class="text-right">
+                                 Permisos
+                                </th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                                @foreach($users as $user)
+                                <tr>
+                                <td scope="row">{{$user->name}}</td>
+                                </tr>
+                                @endforeach
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
+
+
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                 <h4>Ususarios</h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                             <th>Usuarios</th>
+                                <th class="text-right">
+                                 Opciones
+                                </th>
+                            </tr>
+                        </thead>
+
+                            <tbody>
+                                @foreach($permisos as $permiso)
+                                 <tr>
+                                 <td scope="row">{{$permiso->name}}</td>
+                                </tr>
+                                  @endforeach
+
+                                
+                             </tbody>
+
+                        
+                            </table>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+            
+    @endsection       
